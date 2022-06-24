84d0b819bf1dc3d159faa435ae41c650df7043cc
euno <seal5945@gmail.com>
2022-06-24 09:39:15 +0900

    downloads.py writing

--- utils/downloads.py
+++ utils/downloads.py
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
 # YOLOv5 🚀 by Ultralytics, GPL-3.0 license
-=======
-# YOLOv5 by Ultralytics, GPL-3.0 license
->>>>>>> 398b673144da8464a02447002def20fcc3a2f949
 """
 Download utils
 """
 
-<<<<<<< HEAD
-import logging
-=======
->>>>>>> 398b673144da8464a02447002def20fcc3a2f949
 import os
 import platform
 import subprocess
@@ -22,18 +14,36 @@ from zipfile import ZipFile
 import requests
 import torch
 
-<<<<<<< HEAD
+
 def gsutil_getsize(url=''):
     # gs://bucket/file size https://cloud.google.com/storage/docs/gsutil/commands/du
     s = subprocess.check_output(f'gsutil du {url}', shell=True).decode('utf-8')
     return eval(s.split(' ')[0]) if len(s) else 0
 
-def safe_download(file, url, url2=None, min_mytes=1E0, error_msg=''):
-    # Attemps to download file from url or url2, checks and removes incomplete downloads < min_bytes
-    from utils.general import LOGGER
-=======
 
-def gsutil_getsize(url=''):
-    # gs://bucket/file size https://cloud.google.com/storage/docs/gsutil/commands/du
-    s = subprocess.check_output(f'gsutil du {url}', shell=True).decode('utf-8')
->>>>>>> 398b673144da8464a02447002def20fcc3a2f949
+def safe_download(file, url, url2=None, min_bytes=1E0, error_msg=''):
+    # Attemps to download file from url or url2, checks and removes incomplete downloads < min_bytes
+    file = Path(file)
+    assert_msg = f"Downloaded file '{file}' does not exist or size is < min_bytes={min_bytes}"
+    try:  # url1
+        print(f'Downloading {url} to {file}...')
+        torch.hub.download_url_to_file(url, str(file))
+        assert file.exists() and file.stat().st_size > min_bytes, assert_msg  # check
+    except Exception as e:  # url2
+        file.unlink(missing_ok=True)  # remove partial downloads
+        print(f'ERROR: {e}\nRe-attempting {url2 or url} to {file}...')
+        os.system(f"curl -L '{url2 or url}' -o '{file}' --retry 3 -C -")  # curl download, retry and resume on fail
+    finally:
+        if not file.exists() or file.stat().st_size < min_bytes:  # check
+            file.unlink(missing_ok=True)
+            print(f"ERROR: {assert_msg}\n{error_msg}")
+        print('')
+
+
+def attempt_download(file, repo='ultralytics/yolov5'):  #from utils.downloads import *; attempt_download()
+    # Attempt file download if does not exist
+    file = Path(str(file).strip().replace("'", ''))
+
+    if not file.exists():
+        # URL specified
+        name = Path(urllib.parse.unquote(str(file))).name  # decode '%2F' to '/' etc.
